05fec9d6ce4319ee37a7e9ac48022e74b80baaf7
elkraps <dobroisreal@mail.ru>
2023-06-29 08:34:58 +0300

    Initial Commit

---
+++ WhatsGoingOn/WhatsGoingOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsGoingOnApp.swift
+//  WhatsGoingOn
+//
+//  Created by Vlad Dzirko on 29.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatsGoingOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
